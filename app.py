57729c5dde5b9c3ae9ddaa0240c950e607838220
Akshaye <akshayanil770@gmail.com>
2025-07-30 15:28:20 -0400

    Initial commit - add Flask CRUD app

--- app.py
+++ app.py
@@ -9,7 +9,7 @@ app.secret_key = 'flash message'
 
 app.config['MYSQL_HOST'] = 'localhost'
 app.config['MYSQL_USER'] = 'root'
-app.config['MYSQL_PASSWORD'] = ''
+app.config['MYSQL_PASSWORD'] = 'root'
 app.config['MYSQL_DB'] = 'python_crud'
 
 mysql = MySQL(app)
